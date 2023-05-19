da76a17c9be03ab08717749e577d383047ed2a78
Aniket Debnath <36085832+ADebnath98@users.noreply.github.com>
2023-05-19 20:38:43 +0530

    Add files via upload

---
+++ 577. Employee Bonus.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT Employee.name,
+Bonus.bonus
+FROM Employee
+LEFT 
+JOIN Bonus
+ON Employee.empId = Bonus.empId
+WHERE Bonus.bonus < 1000 OR Bonus.bonus is null
